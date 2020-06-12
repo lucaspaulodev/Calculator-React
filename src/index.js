add7a98e0ca53853a672bb62a6e54caadcf494f7
Lucas Paulo <lucaspaulodev@gmail.com>
2020-06-12 06:21:27 -0300

    React Calculator first version
    Uploading the first version of React Calculator

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/Calculator';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>
+      React Calculator ⚛
+    </h1>
+    <Calculator/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
